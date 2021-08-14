b0799918868cc08c928c01afc35092b3feb93cfe
SUDA Mikihrio <s@sudame.net>
2021-08-14 12:19:26 +0900

    remove comma

--- tools/run-alp.sh
+++ tools/run-alp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo alp json --file=/var/log/envoy/access.log -r -m "/api/admin/clarifications/.+,/admin/clarifications/.+,/api/contestant/benchmark_jobs/.+,/contestant/benchmark_jobs/.+," --sort=sum
+sudo alp json --file=/var/log/envoy/access.log -r -m "/api/admin/clarifications/.+,/admin/clarifications/.+,/api/contestant/benchmark_jobs/.+,/contestant/benchmark_jobs/.+" --sort=sum
