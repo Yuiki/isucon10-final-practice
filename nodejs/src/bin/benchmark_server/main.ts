8d294bb72b3f6d3e04cd4101c913ac7e70bb082a
Yuiki <10101661+Yuiki@users.noreply.github.com>
2021-08-14 11:40:49 +0900

    apply prettier

--- nodejs/src/bin/benchmark_server/main.ts
+++ nodejs/src/bin/benchmark_server/main.ts
@@ -1,49 +1,75 @@
-import mysql from "promise-mysql";
-import util from "util";
-import strftime from 'strftime'
-import { getDB, secureRandom, notifier, convertDateToTimestamp } from "../../app";
-import grpc, { callError, makeGenericClientConstructor } from "grpc";
-import BenchmarkQueue from "../../../proto/xsuportal/services/bench/receiving_grpc_pb";
-import { ReceiveBenchmarkJobRequest, ReceiveBenchmarkJobResponse } from "../../../proto/xsuportal/services/bench/receiving_pb";
-import BenchmarkReport from "../../../proto/xsuportal/services/bench/reporting_grpc_pb";
-import { BenchmarkJob } from "../../../proto/xsuportal/resources/benchmark_job_pb";
-import { ReportBenchmarkResultRequest, ReportBenchmarkResultResponse } from "../../../proto/xsuportal/services/bench/reporting_pb";
-import { Timestamp } from "../../../proto/google/protobuf/timestamp_pb";
-import { start } from "repl";
+import mysql from "promise-mysql"
+import util from "util"
+import strftime from "strftime"
+import {
+  getDB,
+  secureRandom,
+  notifier,
+  convertDateToTimestamp,
+} from "../../app"
+import grpc, { callError, makeGenericClientConstructor } from "grpc"
+import BenchmarkQueue from "../../../proto/xsuportal/services/bench/receiving_grpc_pb"
+import {
+  ReceiveBenchmarkJobRequest,
+  ReceiveBenchmarkJobResponse,
+} from "../../../proto/xsuportal/services/bench/receiving_pb"
+import BenchmarkReport from "../../../proto/xsuportal/services/bench/reporting_grpc_pb"
+import { BenchmarkJob } from "../../../proto/xsuportal/resources/benchmark_job_pb"
+import {
+  ReportBenchmarkResultRequest,
+  ReportBenchmarkResultResponse,
+} from "../../../proto/xsuportal/services/bench/reporting_pb"
+import { Timestamp } from "../../../proto/google/protobuf/timestamp_pb"
+import { start } from "repl"
 
-const sleep = util.promisify(setTimeout);
+const sleep = util.promisify(setTimeout)
 
 class BenchmarkQueueService implements BenchmarkQueue.IBenchmarkQueueServer {
   receiveBenchmarkJob(
     call: grpc.ServerUnaryCall<ReceiveBenchmarkJobRequest>,
-    callback: grpc.sendUnaryData<ReceiveBenchmarkJobResponse>,
+    callback: grpc.sendUnaryData<ReceiveBenchmarkJobResponse>
   ) {
-    this.receiveBenchmarkJobPromise(call.request).then((res) => {
-      callback(null, res);
-    }).catch((e) => {
-      callback(e, null);
-    });
+    this.receiveBenchmarkJobPromise(call.request)
+      .then((res) => {
+        callback(null, res)
+      })
+      .catch((e) => {
+        callback(e, null)
+      })
   }
 
-  async receiveBenchmarkJobPromise(request: ReceiveBenchmarkJobRequest): Promise<ReceiveBenchmarkJobResponse> {
-    const db = await getDB();
+  async receiveBenchmarkJobPromise(
+    request: ReceiveBenchmarkJobRequest
+  ): Promise<ReceiveBenchmarkJobResponse> {
+    const db = await getDB()
 
-    let jobHandle = null;
+    let jobHandle = null
     try {
       while (true) {
-        await db.beginTransaction();
-        const job = await this.pollBenchmarkJobs(db);
+        await db.beginTransaction()
+        const job = await this.pollBenchmarkJobs(db)
         if (job == null) {
-          await db.rollback();
-          break;
+          await db.rollback()
+          break
         }
-        const [gotLock] = await db.query('SELECT 1 FROM `benchmark_jobs` WHERE `id` = ? AND `status` = ? FOR UPDATE', [job.id, BenchmarkJob.Status.PENDING]);
+        const [gotLock] = await db.query(
+          "SELECT 1 FROM `benchmark_jobs` WHERE `id` = ? AND `status` = ? FOR UPDATE",
+          [job.id, BenchmarkJob.Status.PENDING]
+        )
         if (gotLock != null) {
-          const handle = secureRandom(16);
-          await db.query('UPDATE `benchmark_jobs` SET `status` = ?, handle = ? WHERE `id` = ? AND `status` = ? LIMIT 1', [
-            BenchmarkJob.Status.SENT, handle, job.id, BenchmarkJob.Status.PENDING,
-          ]);
-          const [contest] = await db.query('SELECT `contest_starts_at` FROM `contest_config` LIMIT 1');
+          const handle = secureRandom(16)
+          await db.query(
+            "UPDATE `benchmark_jobs` SET `status` = ?, handle = ? WHERE `id` = ? AND `status` = ? LIMIT 1",
+            [
+              BenchmarkJob.Status.SENT,
+              handle,
+              job.id,
+              BenchmarkJob.Status.PENDING,
+            ]
+          )
+          const [contest] = await db.query(
+            "SELECT `contest_starts_at` FROM `contest_config` LIMIT 1"
+          )
           jobHandle = {
             jobId: job.id,
             handle,
@@ -51,37 +77,39 @@ class BenchmarkQueueService implements BenchmarkQueue.IBenchmarkQueueServer {
             contestStartedAt: contest.contest_started_at,
             jobCreatedAt: job.created_at,
           }
-          await db.commit();
-          break;
+          await db.commit()
+          break
         } else {
-          await db.rollback();
-          continue;
+          await db.rollback()
+          continue
         }
       }
-      const response = new ReceiveBenchmarkJobResponse();
+      const response = new ReceiveBenchmarkJobResponse()
       if (jobHandle != null) {
-        console.log(`Dequeued: job_handle=${JSON.stringify(jobHandle)}`);
-        const jobHandleResource = new ReceiveBenchmarkJobResponse.JobHandle();
-        jobHandleResource.setHandle(jobHandle.handle);
-        jobHandleResource.setJobId(jobHandle.jobId);
+        console.log(`Dequeued: job_handle=${JSON.stringify(jobHandle)}`)
+        const jobHandleResource = new ReceiveBenchmarkJobResponse.JobHandle()
+        jobHandleResource.setHandle(jobHandle.handle)
+        jobHandleResource.setJobId(jobHandle.jobId)
         if (jobHandle.contestStartedAt) {
-          const contestStartedAt = convertDateToTimestamp(jobHandle.contestStartedAt);
-          jobHandleResource.setContestStartedAt(contestStartedAt);
+          const contestStartedAt = convertDateToTimestamp(
+            jobHandle.contestStartedAt
+          )
+          jobHandleResource.setContestStartedAt(contestStartedAt)
         }
         if (jobHandle.jobCreatedAt) {
-          const jobCreatedAt = convertDateToTimestamp(jobHandle.jobCreatedAt);
-          jobHandleResource.setJobCreatedAt(jobCreatedAt);
+          const jobCreatedAt = convertDateToTimestamp(jobHandle.jobCreatedAt)
+          jobHandleResource.setJobCreatedAt(jobCreatedAt)
         }
-        jobHandleResource.setTargetHostname(jobHandle.targetHostName);
-        response.setJobHandle(jobHandleResource);
+        jobHandleResource.setTargetHostname(jobHandle.targetHostName)
+        response.setJobHandle(jobHandleResource)
       }
-      await db.commit();
-      return response;
+      await db.commit()
+      return response
     } catch (e) {
-      console.error(e);
-      throw e;
+      console.error(e)
+      throw e
     } finally {
-      await db.release();
+      await db.release()
     }
   }
 
@@ -89,87 +117,108 @@ class BenchmarkQueueService implements BenchmarkQueue.IBenchmarkQueueServer {
     let job = null
     for (let i = 0; i < 10; i++) {
       if (i >= 1) {
-        await sleep(50);
+        await sleep(50)
       }
-      [job] = await db.query('SELECT * FROM `benchmark_jobs` WHERE `status` = ? ORDER BY `id` LIMIT 1', [BenchmarkJob.Status.PENDING]);
+      ;[job] = await db.query(
+        "SELECT * FROM `benchmark_jobs` WHERE `status` = ? ORDER BY `id` LIMIT 1",
+        [BenchmarkJob.Status.PENDING]
+      )
       if (job) {
-        break;
+        break
       }
     }
-    return job;
+    return job
   }
 }
 
 class BenchmarkReportService implements BenchmarkReport.IBenchmarkReportServer {
   reportBenchmarkResult(
-    call: grpc.ServerDuplexStream<ReportBenchmarkResultRequest, ReportBenchmarkResultResponse>,
+    call: grpc.ServerDuplexStream<
+      ReportBenchmarkResultRequest,
+      ReportBenchmarkResultResponse
+    >
   ) {
     call.on("data", async (request: ReportBenchmarkResultRequest) => {
-      const response = await this.reportBenchmarkResultPromise(request);
-      call.write(response);
-    });
+      const response = await this.reportBenchmarkResultPromise(request)
+      call.write(response)
+    })
     call.on("end", () => {
-      call.end();
-    });
+      call.end()
+    })
   }
 
-  async reportBenchmarkResultPromise(request: ReportBenchmarkResultRequest): Promise<ReportBenchmarkResultResponse> {
-    const db = await getDB();
-    const notify = notifier;
+  async reportBenchmarkResultPromise(
+    request: ReportBenchmarkResultRequest
+  ): Promise<ReportBenchmarkResultResponse> {
+    const db = await getDB()
+    const notify = notifier
 
     if (!request.hasResult()) {
-      throw new Error("Invalid Argument result required");
+      throw new Error("Invalid Argument result required")
     }
 
     try {
-      await db.beginTransaction();
+      await db.beginTransaction()
       const [job] = await db.query(
-        'SELECT * FROM `benchmark_jobs` WHERE `id` = ? AND `handle` = ? LIMIT 1 FOR UPDATE',
-        [request.getJobId(), request.getHandle(),]
-      );
+        "SELECT * FROM `benchmark_jobs` WHERE `id` = ? AND `handle` = ? LIMIT 1 FOR UPDATE",
+        [request.getJobId(), request.getHandle()]
+      )
 
       if (job == null) {
-        await db.rollback();
-        console.error(`Job not found: job_id=${request.getJobId()}, handle=${request.getHandle().toString()}`);
-        throw new Error(`Job ${request.getJobId()} not found or handle is wrong`);
+        await db.rollback()
+        console.error(
+          `Job not found: job_id=${request.getJobId()}, handle=${request
+            .getHandle()
+            .toString()}`
+        )
+        throw new Error(
+          `Job ${request.getJobId()} not found or handle is wrong`
+        )
       }
 
       if (request.getResult()?.getFinished()) {
-        console.debug(`${request.getJobId()}: save as finished`);
-        await this.saveAsFinished(job, request, db);
-        await db.commit();
-        notifier.notifyBenchmarkJobFinished(job, db);
+        console.debug(`${request.getJobId()}: save as finished`)
+        await this.saveAsFinished(job, request, db)
+        await db.commit()
+        notifier.notifyBenchmarkJobFinished(job, db)
       } else {
-        console.debug(`${request.getJobId()}: save as running`);
-        await this.saveAsRunning(job, request, db);
-        await db.commit();
+        console.debug(`${request.getJobId()}: save as running`)
+        await this.saveAsRunning(job, request, db)
+        await db.commit()
       }
-      const response = new ReportBenchmarkResultResponse();
-      response.setAckedNonce(request.getNonce());
-      return response;
+      const response = new ReportBenchmarkResultResponse()
+      response.setAckedNonce(request.getNonce())
+      return response
     } catch (e) {
-      await db.rollback();
-      throw e;
+      await db.rollback()
+      throw e
     } finally {
-      await db.release();
+      await db.release()
     }
   }
 
-  async saveAsFinished(job: any, request: ReportBenchmarkResultRequest, db: mysql.PoolConnection) {
+  async saveAsFinished(
+    job: any,
+    request: ReportBenchmarkResultRequest,
+    db: mysql.PoolConnection
+  ) {
     if (job.started_at == null || job.finished_at != null) {
-      await db.rollback();
-      throw new Error(`Job ${request.getJobId()} has already finished or has not started yet`);
+      await db.rollback()
+      throw new Error(
+        `Job ${request.getJobId()} has already finished or has not started yet`
+      )
     }
-    const markedAtTimestamp = request.getResult()?.getMarkedAt();
+    const markedAtTimestamp = request.getResult()?.getMarkedAt()
     if (!markedAtTimestamp) {
-      await db.rollback();
-      throw new Error("marked_at is required");
+      await db.rollback()
+      throw new Error("marked_at is required")
     }
 
-    const markedAt = formatDate(markedAtTimestamp);
+    const markedAt = formatDate(markedAtTimestamp)
 
-    const result = request.getResult();
-    await db.query(`
+    const result = request.getResult()
+    await db.query(
+      `
         UPDATE benchmark_jobs SET
           status = ?,
           score_raw = ?,
@@ -181,25 +230,38 @@ class BenchmarkReportService implements BenchmarkReport.IBenchmarkReportServer {
         WHERE id = ?
         LIMIT 1
     `,
-      [BenchmarkJob.Status.FINISHED, result?.getScoreBreakdown()?.getRaw(), result?.getScoreBreakdown()?.getDeduction(), result?.getPassed(), result?.getReason(), markedAt, request.getJobId()]
-    );
+      [
+        BenchmarkJob.Status.FINISHED,
+        result?.getScoreBreakdown()?.getRaw(),
+        result?.getScoreBreakdown()?.getDeduction(),
+        result?.getPassed(),
+        result?.getReason(),
+        markedAt,
+        request.getJobId(),
+      ]
+    )
   }
 
-  async saveAsRunning(job: any, request: ReportBenchmarkResultRequest, db: mysql.PoolConnection) {
-    const markedAtTimestamp = request.getResult()?.getMarkedAt();
+  async saveAsRunning(
+    job: any,
+    request: ReportBenchmarkResultRequest,
+    db: mysql.PoolConnection
+  ) {
+    const markedAtTimestamp = request.getResult()?.getMarkedAt()
     if (!markedAtTimestamp) {
-      await db.rollback();
-      throw new Error("marked_at is required");
+      await db.rollback()
+      throw new Error("marked_at is required")
     }
-    const markedAt = formatDate(markedAtTimestamp);
-    const result = request.getResult();
-    let startedAt = null;
+    const markedAt = formatDate(markedAtTimestamp)
+    const result = request.getResult()
+    let startedAt = null
     if (job.started_at) {
-      startedAt = new Date(job.started_at);
+      startedAt = new Date(job.started_at)
     } else {
-      startedAt = markedAt;
+      startedAt = markedAt
     }
-    await db.query(`
+    await db.query(
+      `
         UPDATE benchmark_jobs SET
           status = ?,
           score_raw = NULL,
@@ -212,29 +274,37 @@ class BenchmarkReportService implements BenchmarkReport.IBenchmarkReportServer {
         WHERE id = ?
         LIMIT 1
     `,
-      [
-        BenchmarkJob.Status.RUNNING,
-        startedAt,
-        request.getJobId()]
-    );
+      [BenchmarkJob.Status.RUNNING, startedAt, request.getJobId()]
+    )
   }
 }
 
 function formatDate(timestamp: Timestamp) {
-  const date = new Date(timestamp.getSeconds() * 1000);
-  const datestr = `${date.getUTCFullYear()}-${String(date.getUTCMonth() + 1).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")} ${String(date.getUTCHours()).padStart(2, "0")}:${String(date.getUTCMinutes()).padStart(2, "0")}:${String(date.getUTCSeconds()).padStart(2, "0")}.${String(timestamp.getNanos() / 1000).padStart(6,"0")}`;
-  return datestr;
+  const date = new Date(timestamp.getSeconds() * 1000)
+  const datestr = `${date.getUTCFullYear()}-${String(
+    date.getUTCMonth() + 1
+  ).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")} ${String(
+    date.getUTCHours()
+  ).padStart(2, "0")}:${String(date.getUTCMinutes()).padStart(2, "0")}:${String(
+    date.getUTCSeconds()
+  ).padStart(2, "0")}.${String(timestamp.getNanos() / 1000).padStart(6, "0")}`
+  return datestr
 }
 
 function main() {
-  const server = new grpc.Server();
-  const port = process.env["PORT"] ?? 50051;
+  const server = new grpc.Server()
+  const port = process.env["PORT"] ?? 50051
 
-
-  server.addService<BenchmarkQueue.IBenchmarkQueueServer>(BenchmarkQueue.BenchmarkQueueService, new BenchmarkQueueService());
-  server.addService<BenchmarkReport.IBenchmarkReportServer>(BenchmarkReport.BenchmarkReportService, new BenchmarkReportService());
-  server.bind(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure());
-  server.start();
+  server.addService<BenchmarkQueue.IBenchmarkQueueServer>(
+    BenchmarkQueue.BenchmarkQueueService,
+    new BenchmarkQueueService()
+  )
+  server.addService<BenchmarkReport.IBenchmarkReportServer>(
+    BenchmarkReport.BenchmarkReportService,
+    new BenchmarkReportService()
+  )
+  server.bind(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure())
+  server.start()
 }
 
-main();
+main()
