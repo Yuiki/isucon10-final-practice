6b174eebd07415a993eb26834d989e7a9dc30ec0
haigyo <haigyo@example.com>
2021-08-14 08:50:18 +0000

    fix: tools

--- tools/run-alp.sh
+++ tools/run-alp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo alp json --file=/var/log/envoy/access.log -r -m "/api/admin/clarifications/.+,/admin/clarifications/.+,/api/contestant/benchmark_jobs/.+,/contestant/benchmark_jobs/.+" --sort=sum
-bash ./create-issue-with-alp.sh
+bash `dirname $0`/create-issue-with-alp.sh
