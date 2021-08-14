e4c80d7dc4ff94ff64d50052efdab34d343113d8
Yuiki <10101661+Yuiki@users.noreply.github.com>
2021-08-14 11:48:36 +0900

    add pre-bench

---
+++ tools/pre-bench.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo truncate -s 0 -c /var/log/envoy/access.log
+sudo truncate -s 0 -c /var/log/mysql/mysql-slow.log
+
+git pull
+cd ~/webapp/nodejs && npm install
+
+sudo systemctl restart xsuportal-api-nodejs.service
+sudo systemctl restart xsuportal-web-nodejs.service
+
+sudo systemctl restart envoy
+sudo systemctl restart mysql
