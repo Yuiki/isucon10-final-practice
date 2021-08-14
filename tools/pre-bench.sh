cb56d07270584961c918d90d3859259b596a311f
Yuiki <10101661+Yuiki@users.noreply.github.com>
2021-08-14 13:46:47 +0900

    fix pre-bench

--- tools/pre-bench.sh
+++ tools/pre-bench.sh
@@ -5,8 +5,8 @@ sudo truncate -s 0 -c /var/log/mysql/mysql-slow.log
 git pull
 cd ~/webapp/nodejs && npm install
 
+sudo systemctl restart mysql
+sudo systemctl restart envoy
+
 sudo systemctl restart xsuportal-api-nodejs.service
 sudo systemctl restart xsuportal-web-nodejs.service
-
-sudo systemctl restart envoy
-sudo systemctl restart mysql
